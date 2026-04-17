3edb7a77046c264081a57fe852780c4276b318e4
SSS Vishnu Yadavalli <vishnu.sss.yadavalli@gmail.com>
2026-04-17 18:30:20 -0500

    Refactor image upload and prediction process

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
+from PIL import Image
 from model_helper import predict
 
 st.title("Vehicle Damage Detection")
 
-uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
-    image_path = "temp_file.jpg"
-    with open(image_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-        st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
-        prediction = predict(image_path)
-        st.info(f"Predicted Class: {prediction}")
+    image = Image.open(uploaded_file).convert("RGB")
+
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    with st.spinner("Analyzing image..."):
+        prediction = predict(image)
+
+    st.success(f"Predicted Class: {prediction}")
